b29246af606845e39d3e04073912782ffa1c10dd
BodyaHromoy <121422184+BodyaHromoy@users.noreply.github.com>
2023-01-13 15:49:57 +0600

    Проектная работа

---
+++ bilet_practica/bilet_practica/bilet_practica.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    int m;
+    cout << "Введите количество строк: ";
+    cin >> n;
+    cout << "Введите количество столбцов: ";
+    cin >> m;
+    char** mass = new char*[n];
+    for (int i = 0; i < n; ++i)
+        mass[i] = new char[m];
+    int i, j, c(-1);
+    char* word = new char[n * m];
+    cout << "Введите фразу без пробелов: ";
+    cin >> word;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            c++;
+            mass[i][j] = word[c];
+            cout << mass[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Шифр: ";
+    for (j = 0; j < n; j++) {
+        for (i = 0; i < m; i++) {
+            cout << mass[i][j];
+        }
+    }
+    delete[]mass;
+    delete[]word;
+    cout << endl;
+}
